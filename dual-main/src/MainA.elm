13a3fc885d765dcec6462ae526f1813b79ad2722
Markus Laire <malaire@gmail.com>
2018-10-28 00:28:07 +0300

    add dual-main example

---
+++ dual-main/src/MainA.elm
@@ -0,0 +1,65 @@
+module MainA exposing (main)
+
+import Browser
+import Html exposing (..)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    ()
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( ()
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Never
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Never ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "Hello from MainA" ]
