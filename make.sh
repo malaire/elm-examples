2048d0c61d99c46c30cf934b97395bc96c3cb631
Markus Laire <malaire@gmail.com>
2018-10-28 00:27:41 +0300

    compile all source files, not just Main

--- make.sh
+++ make.sh
@@ -10,7 +10,7 @@ set -e
 JS=elm.js
 MIN=elm.min.js
 
-elm make src/Main.elm --optimize --output=$JS
+elm make src/*.elm --optimize --output=$JS
 
 uglifyjs $JS --compress \
   'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
